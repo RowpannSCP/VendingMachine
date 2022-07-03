190a338a03ce7187324828d5b45cd4e1211254a2
moddedmcplayer <76910334+moddedmcplayer@users.noreply.github.com>
2022-07-03 18:12:52 +0200

    funni

--- VendingMachine/Config.cs
+++ VendingMachine/Config.cs
@@ -15,8 +15,8 @@ namespace VendingMachine
         [Description("Will give items even if there is no space in the inventory.")]
         public bool IgnoreSpace { get; set; } = false;
 
-        [Description("The deals, use ItemType.None for random item")]
-        public List<DealModel> Deals = new List<DealModel>()
+        [Description("The deals, use ItemType.None for random item. A random item is chosen from the list.")]
+        public List<DealModel> Deals { get; set; } = new List<DealModel>()
         {
             new DealModel()
             {
